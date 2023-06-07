69bbd7d1b093622c9e1a67e0982d06c3f7c2dfb1
Kelly Bernacki <kellyq312@yahoo.com>
2023-06-07 15:31:02 -0500

    first commit

---
+++ starting_code/src/App.js
@@ -0,0 +1,30 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+
+
+// Add react-router-dom imports
+import {Routes, Route } from "react-router-dom";
+
+// create router with JSX Route elements
+const appRouter = "REPLACE ME";
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Root />}>
+        <Route index element={<HomePage />}/>
+        <Route path=':type' element={<HomePage />} />
+        <Route path=":type/:id" element={<PetDetailsPage />}/>
+        <Route path="search" element={<SearchPage />} />
+        <Route path="/pet-details-not-found" element={<PetDetailsNotFound />} />
+        </Route>
+       </Routes>
+    </>
+  );
+}
+
+export default App;
